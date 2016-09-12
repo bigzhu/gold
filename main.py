395aa432a6a68468d4dbffdf5c24573b2bcef916
bigzhu <vermiliondun@gmail.com>
2016-09-12 08:29:02 +0800

    统一输出UNIT’

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ with open('config.ini', 'r') as cfg_file:
     SELL_AT = float(config.get('config', 'SELL_AT'))
     SELL_AT1 = float(config.get('config', 'SELL_AT1'))
 
+UNIT = (ATR / 10)
+
 
 def readGodDatas():
     high = []
@@ -27,65 +29,66 @@ def readGodDatas():
 
 
 def getSell(high, low):
-    unit = (ATR / 10)
 
-    sell = high - unit
+    sell = high - UNIT
     sl = high + ATR
     if SELL_AT != 0:  # 买了
         if SELL_AT1 != 0:  # 买了追加
-            sell_at1_sl = SELL_AT + unit + ATR + (ATR / 2)
+            sell_at1_sl = SELL_AT + UNIT + ATR + (ATR / 2)
             sell_at1_tp = low + ATR
             print "sell_at(1)=%.3f S/L=%.3f T/P=%.3f" % (SELL_AT1, sell_at1_sl, sell_at1_tp)
-        else: # 没追加
+        else:  # 没追加
             sell1 = SELL_AT + (ATR / 2)
-            sell1_sl = sell1 + unit + ATR + (ATR / 2)
+            sell1_sl = sell1 + UNIT + ATR + (ATR / 2)
             sell1_tp = low + ATR
             print "sell(1)=%.3f S/L=%.3f T/P=%.3f" % (sell1, sell1_sl, sell1_tp)
 
-        sell_at_sl = SELL_AT + unit + ATR
+        sell_at_sl = SELL_AT + UNIT + ATR
         sell_at_tp = low + (ATR / 2)
         print "sell_at=%.3f S/L=%.3f T/P=%.3f" % (SELL_AT, sell_at_sl, sell_at_tp)
         if SELL_AT1 == 0:  # 没有买2保时再追加
-            sell = appendSell(SELL_AT, unit, 1)
-            sell = appendSell(sell, unit, 2)
-            sell = appendSell(sell, unit, 3)
+            sell = appendSell(SELL_AT, UNIT, 1)
+            sell = appendSell(sell, UNIT, 2)
+            sell = appendSell(sell, UNIT, 3)
     else:
         print "sell0=%.3f S/L=%.3f" % (sell, sl)
 
-    # sell = appendSell(sell, unit, 1)
-    # sell = appendSell(sell, unit, 2)
-    # sell = appendSell(sell, unit, 3)
+    # sell = appendSell(sell, UNIT, 1)
+    # sell = appendSell(sell, UNIT, 2)
+    # sell = appendSell(sell, UNIT, 3)
 
 
-def appendSell(value, unit, count):
-    sell = value - unit
+def appendSell(value, UNIT, count):
+    sell = value - UNIT
     sl = value + ATR
     print "sell%s=%.3f S/L%s=%.3f" % (count, sell, count, sl)
     return sell
 
 
 def getBuy(low):
-    unit = (ATR / 10)
-    buy = low + unit
+    UNIT = (ATR / 10)
+    buy = low + UNIT
     buy_stop = low - ATR
     # print "buy_-1=%.3f buy__stop-1=%.3f" % (buy - (ATR / 2), buy_stop - (ATR / 2))
     print "buy_0=%.3f buy__stop0=%.3f" % (buy, buy_stop)
-    # buy = appendBuy(buy, unit, 1)
-    # buy = appendBuy(buy, unit, 2)
-    # buy = appendBuy(buy, unit, 3)
+    # buy = appendBuy(buy, UNIT, 1)
+    # buy = appendBuy(buy, UNIT, 2)
+    # buy = appendBuy(buy, UNIT, 3)
 
 
-def appendBuy(value, unit, count):
-    buy = value + unit
+def appendBuy(value, UNIT, count):
+    buy = value + UNIT
     buy_stop = buy - ATR
     print "buy_%s=%.3f buy__stop%s=%.3f" % (count, buy, count, buy_stop)
     return buy
 
 
 def main():
+    print 'UNIT=%.3f' % UNIT
     high, low = readGodDatas()
     high = max(high)
     low = min(low)
+
     getSell(high, low)
     print ''
     print ''
