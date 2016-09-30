9179fdfd3af1c15bb400d7c3fd021b9d497d806e
bigzhu <vermiliondun@gmail.com>
2016-10-01 00:07:33 +0800

    重新梳理,定好规则

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+'''
+卖出：10日内最高-浮动(FLUC)
+止盈(T/P): + 2*ATR
+止损(S/L): + UNIT + ATR + (ATR / 2)
+救卖出: +(ATR/2)
+追加: - UNIT 4-5 个
+
+
+'''
 import sys
 import ConfigParser
 config = ConfigParser.ConfigParser()
@@ -8,8 +17,12 @@ with open('config.ini', 'r') as cfg_file:
     ATR = float(config.get('config', 'ATR'))
     LONG_DAY = int(config.get('config', 'LONG_DAY'))
     SHORT_DAY = int(config.get('config', 'SHORT_DAY'))
+    SELL_SAVE_AT = float(config.get('config', 'SELL_SAVE_AT'))
     SELL_AT = float(config.get('config', 'SELL_AT'))
     SELL_AT1 = float(config.get('config', 'SELL_AT1'))
+    SELL_AT2 = float(config.get('config', 'SELL_AT2'))
+    SELL_AT3 = float(config.get('config', 'SELL_AT3'))
+    SELL_AT4 = float(config.get('config', 'SELL_AT4'))
 
 UNIT = (ATR / 10)
 FLUC = (ATR / 4)  # 购买浮动
@@ -37,46 +50,82 @@ S_HIGH = max(high)
 S_LOW = min(low)
 
 
-def getSellTp(sell_at):
-    # return sell_at - UNIT - ATR - (ATR / 2)
-    return sell_at - (ATR * 1.3)
+def getSellAt():
+    '''
+    卖出：10日内最高-浮动(FLUC)
+    '''
+    sell_at = L_HIGH - FLUC
+    return sell_at
 
 
-def getSell():
-    sell = L_HIGH - FLUC
-    sl = L_HIGH + ATR
-    if SELL_AT != 0:  # 买了
-        if SELL_AT1 != 0:  # 买了追加
-            sell_at1_sl = SELL_AT + UNIT + ATR + (ATR / 2)
-            sell_at1_tp = getSellTp(SELL_AT1)
-            print "sell_at(1)=%.3f S/L=%.3f T/P=%.3f" % (SELL_AT1, sell_at1_sl, sell_at1_tp)
-            sell = appendSell(SELL_AT1, UNIT, 1)
-            sell = appendSell(sell, UNIT, 2)
-            sell = appendSell(sell, UNIT, 3)
-        else:  # 没追加
-            sell1 = SELL_AT + (ATR / 2)
-            sell1_sl = sell1 + UNIT + ATR + (ATR / 2)
-            sell1_tp = getSellTp(sell1)
-            print "sell(1)=%.3f S/L=%.3f T/P=%.3f" % (sell1, sell1_sl, sell1_tp)
-
-        sell_at_sl = SELL_AT + UNIT + ATR
-        sell_at_tp = getSellTp(SELL_AT)
-        print "sell_at=%.3f S/L=%.3f T/P=%.3f" % (SELL_AT, sell_at_sl, sell_at_tp)
-        if SELL_AT1 == 0:  # 没有买2保时再追加
-            sell = appendSell(SELL_AT, UNIT, 1)
-            sell = appendSell(sell, UNIT, 2)
-            sell = appendSell(sell, UNIT, 3)
-    else:
-        print "sell0=%.3f S/L=%.3f" % (sell, sl)
+def getSellTP(sell_at):
+    '''
+    止盈(T/P): 2*ATR
+    '''
+    return sell_at - (2 * ATR)
 
 
-def appendSell(value, UNIT, count):
-    sell = value - UNIT
-    sl = value + ATR
-    tp = getSellTp(sell)
+def getSellSave(sell_at):
+    '''
+    救卖出: +(ATR/2)
+    '''
+    return sell_at + (ATR / 2)
 
-    print "sell%s=%.3f S/L%s=%.3f T/P=%.3f" % (count, sell, count, sl, tp)
+
+def getSellSL(sell_at):
+    '''
+    止损(S/L): + UNIT + ATR + (ATR / 2)
+    '''
+    return sell_at + UNIT + ATR + (ATR / 2)
+
+
+def appendSell(last_sell_at):
+    '''
+    追加: - UNIT 4-5 个
+    '''
+    sell = last_sell_at - UNIT
     return sell
+
+
+def printSell(name, sell_at):
+    print "%s=%.3f S/L=%.3f T/P=%.3f" % (name, sell_at, getSellSL(sell_at), getSellTP(sell_at))
+
+
+def sell():
+    sell_at = getSellAt()
+    if SELL_AT != 0:  # 买了
+        sell_at = SELL_AT
+    else:
+        sell_at = getSellAt()
+    if SELL_SAVE_AT != 0:  # 买了抄底
+        sell_at_save = SELL_SAVE_AT
+    else:
+        sell_at_save = getSellSave(sell_at)
+    printSell('sell_at_save', sell_at_save)
+    printSell('sell_at', sell_at)
+    if SELL_AT1 != 0:
+        sell_at1 = SELL_AT1
+    else:
+        sell_at1 = appendSell(sell_at)
+    printSell('sell_at1', sell_at1)
+
+    if SELL_AT2 != 0:
+        sell_at2 = SELL_AT2
+    else:
+        sell_at2 = appendSell(sell_at1)
+    printSell('sell_at2', sell_at2)
+
+    if SELL_AT3 != 0:
+        sell_at3 = SELL_AT3
+    else:
+        sell_at3 = appendSell(sell_at2)
+    printSell('sell_at3', sell_at3)
+
+    if SELL_AT4 != 0:
+        sell_at4 = SELL_AT4
+    else:
+        sell_at4 = appendSell(sell_at3)
+    printSell('sell_at4', sell_at4)
 
 
 def getBuy():
@@ -97,7 +146,7 @@ def appendBuy(value, UNIT, count):
 
 
 def main():
-    getSell()
+    sell()
     print ''
     getBuy()
 
