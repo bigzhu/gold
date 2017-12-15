d42c5f2c3669bf6cec3480024ac3ffc18a0e6eff
bigzhu <vermiliondun@gmail.com>
2017-12-15 20:42:40 +0800

    计算封装起来给其他调用

--- gold.py
+++ gold.py
@@ -37,31 +37,50 @@ def getTop():
     return top
 
 
-def main():
-    oper = getOper()
-    atr = getAtr()
-    # top = getTop()
-    print('近4天峰值: %s' % (top / 1000))
-    one_quarter = atr / 4
-    print('购买间隔: %s' % (one_quarter / 1000))
-
+def trade(oper, top, atr):
+    '''
+    计算购入点
+    >>> trade('buy', 1296940, 9800)
+    {'reverse': 1277.34, 'intervals': [1294.49, 1292.04, 1289.59, 1287.14, 1284.69, 1282.24, 1279.79, 1277.34, 1274.89, 1272.44, 1269.99, 1267.54, 1265.09]}
+    '''
     two_atr = 2 * atr
+    one_quarter = atr / 4
     if oper == 'buy':
         reverse = top - two_atr
     else:
         reverse = top + two_atr
-    print('保底: %s' % (reverse / 1000))
-    print('-------------------------------------------------------')
+    result = dict(reverse=reverse / 1000)
+    intervals = []
     tmp = top
     for i in range(1, 14):
         if oper == 'buy':
             tmp = tmp - one_quarter
         else:
             tmp = tmp + one_quarter
-        print(tmp / 1000)
+        intervals.append(tmp / 1000)
+    result['intervals'] = intervals
+    return result
+
+
+def main():
+    '''
+    '''
+    oper = getOper()
+    atr = getAtr()
+    # top = getTop()
+    print('近4天峰值: %s' % (top / 1000))
+    one_quarter = atr / 4
+    print('购买间隔: %s' % (one_quarter / 1000))
+    result = trade(oper, top, atr)
+    result_str = '''保底: %s'
+-------------------------------------------
+''' % result['reverse']
+    for i in result['intervals']:
+        result_str += str(i) + '\n'
+    print(result_str)
 
 
 if __name__ == '__main__':
     main()
-    #import doctest
-    #doctest.testmod(verbose=False, optionflags=doctest.ELLIPSIS)
+    # import doctest
+    # doctest.testmod(verbose=False, optionflags=doctest.ELLIPSIS)
